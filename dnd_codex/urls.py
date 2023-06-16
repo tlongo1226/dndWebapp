13b9cbbd0ff6fc2bea0d5c7edde1b6036b0e2aa5
tlongo1226 <tlongo1226@gmail.com>
2023-06-15 22:34:35 -0500

    attempt to add react

--- dnd_codex/urls.py
+++ dnd_codex/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path("enemies/", views.Enemies, name="Enemies"),
     path("allies/", views.Allies, name="Allies"),
     path("creatures/", views.Creatures, name="Creatures"),
     path("index/", views.index, name="index"),
     path("enemy_detail/<int:enemyID>/", views.enemyDetail, name="enemy_detail"),
+    path('api/journal-entries/', views.journal_entries, name='journal-entries'),
 ]
--- dnd_codex/views.py
+++ dnd_codex/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Creature, Ally, Enemy
+from .models import Creature, Ally, Enemy, JournalEntry
 from django.http import HttpResponse
 from django.template import loader
 from .forms import enemyForm
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def Enemies(request):
@@ -67,3 +69,24 @@ def enemyDetail(request, enemyID):
     enemy = get_object_or_404(Enemy, id= enemyID)
 
     return render(request, "codex/enemy_detail.html", {"enemy":enemy})
+
+
+@csrf_exempt
+def journal_entries(request):
+    if request.method == 'GET':
+        # Retrieve all journal entries
+        entries = JournalEntry.objects.all()
+        # Convert entries to JSON format
+        data = [{'id': entry.id, 'date': entry.date, 'events': entry.events} for entry in entries]
+        # Return the JSON response
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        # Create a new journal entry
+        # Retrieve data from the request body
+        data = json.loads(request.body)
+        # Perform necessary validations and create the entry
+        # ...
+        # Return a success response
+        return JsonResponse({'message': 'Journal entry created.'})
+    else:
+        return JsonResponse({'error': 'Method not allowed.'}, status=405)
